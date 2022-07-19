6d98da4c7fed40e434f83e24defd861046aeb149
bbu9112 <andrei.bubulinca@gmail.com>
2022-07-19 20:01:01 +0300

    Initial commit

---
+++ src/main/java/ro/sda/javaro35/finalProject/SpringSeedApplication.java
@@ -0,0 +1,13 @@
+package ro.sda.javaro35.finalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSeedApplication.class, args);
+    }
+
+}
